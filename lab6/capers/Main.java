5765a95205a8856939234c1f9b5d20f71f55c178
Walter Wu <WYH4444813@gmail.com>
2024-10-17 00:02:29 +0800

    Finish Main

--- lab6/capers/Main.java
+++ lab6/capers/Main.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 import static capers.Utils.*;
 
 /** Canine Capers: A Gitlet Prelude.
- * @author TODO
+ * @author Walter Wu
 */
 public class Main {
     /**
@@ -54,11 +54,12 @@ public class Main {
             break;
         case "dog":
             validateNumArgs("dog", args, 4);
-            // TODO: make a dog
+            CapersRepository.makeDog(args[1],args[2],Integer.parseInt(args[3]));
             break;
         case "birthday":
             validateNumArgs("birthday", args, 2);
             // TODO: celebrate this dog's birthday
+            CapersRepository.celebrateBirthday(args[1]);
             break;
         default:
             exitWithError(String.format("Unknown command: %s", args[0]));
