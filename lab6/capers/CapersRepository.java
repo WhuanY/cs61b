13e4ab6e0c8f865814aabeb8357d5f3106b87545
Walter Wu <WYH4444813@gmail.com>
2024-10-16 19:23:02 +0800

    Finish CapersRepository

--- lab6/capers/CapersRepository.java
+++ lab6/capers/CapersRepository.java
@@ -1,25 +1,26 @@
 package capers;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+
 import static capers.Utils.*;
 
 /** A repository for Capers 
- * @author TODO
+ * @author Walter Wu
  * The structure of a Capers Repository is as follows:
  *
  * .capers/ -- top level folder for all persistent data in your lab12 folder
  *    - dogs/ -- folder containing all of the persistent data for dogs
  *    - story -- file containing the current story
  *
- * TODO: change the above structure if you do something different.
  */
 public class CapersRepository {
     /** Current Working Directory. */
     static final File CWD = new File(System.getProperty("user.dir"));
 
     /** Main metadata folder. */
-    static final File CAPERS_FOLDER = null; // TODO Hint: look at the `join`
-                                            //      function in Utils
+    static final File CAPERS_FOLDER = Utils.join(CWD,".capers");
 
     /**
      * Does required filesystem operations to allow for persistence.
@@ -31,7 +32,24 @@ public class CapersRepository {
      *    - story -- file containing the current story
      */
     public static void setupPersistence() {
-        // TODO
+        if (!CAPERS_FOLDER.exists()) {
+            CAPERS_FOLDER.mkdir();
+        }
+        File dogsDir = Utils.join(CAPERS_FOLDER,"dogs");
+        if (!dogsDir.exists()) {
+            dogsDir.mkdir();
+        }
+
+        File storyFile = Utils.join(CAPERS_FOLDER, "story");
+        try {
+            if (!storyFile.exists()) {
+                storyFile.createNewFile();
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     /**
@@ -39,8 +57,12 @@ public class CapersRepository {
      * to a file called `story` in the .capers directory.
      * @param text String of the text to be appended to the story
      */
-    public static void writeStory(String text) {
-        // TODO
+    public static void writeStory(String text) throws IOException {
+        File storyFile = Utils.join(CAPERS_FOLDER, "story");
+        if (!storyFile.exists()) {
+            System.out.println("story files not found, check if you have benn set all things up yet");
+        }
+        Utils.writeContents(storyFile, text);
     }
 
     /**
@@ -49,7 +71,10 @@ public class CapersRepository {
      * Also prints out the dog's information using toString().
      */
     public static void makeDog(String name, String breed, int age) {
-        // TODO
+        Dog newDog = new Dog(name, breed, age);
+        System.out.println(newDog.toString());
+        File outFile = Utils.join(CAPERS_FOLDER,"dogs",name);
+        writeObject(outFile, newDog);
     }
 
     /**
@@ -59,6 +84,11 @@ public class CapersRepository {
      * @param name String name of the Dog whose birthday we're celebrating.
      */
     public static void celebrateBirthday(String name) {
-        // TODO
+        File inFile = Utils.join(CAPERS_FOLDER,"dogs",name);
+        if (!inFile.exists()) {
+            System.out.println("The dog you are celebrating doesn't exist! Try a new Dog");
+        }
+        Dog birthdayDog  = readObject(inFile, Dog.class);
+        birthdayDog.haveBirthday();
     }
 }
